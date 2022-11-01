f302297ee7619fa4e1ce4274ca9f729f522e9749
sarahmoore <sarahnmoore19@gmail.com>
2022-11-01 03:14:31 -0400

    created bubbleSort algorithm, both iterative and recursive

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,43 @@
 
-function bubbleSort(arr) {
+function bubbleSort1(arr) {
+  let count = 1;
+  while (count > 0) {
+    count = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > arr[i + 1]) {
+      count++;
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+    }
+    }
+    console.log(arr.join(","));
 
-    // Iterate through the array
+  }
+  return;
 
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
+
 
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
 
+}
+
+function bubbleSort2(arr) {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[i + 1]) {
+      count++;
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+    }
+  }
+  console.log(arr.join(","));
+
+ if (!count) return;
+ return bubbleSort(arr);
 }
 
 module.exports = bubbleSort;
